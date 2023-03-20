66a2f0c38aef7b0f555dfe018ffdd390bbd93061
mecreal <annoncecbh@gmail.com>
2023-03-20 14:15:54 -0700

    3

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= z)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
