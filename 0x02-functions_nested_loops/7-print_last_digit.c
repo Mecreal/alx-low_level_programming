559917f308b06523568a7ed5013d1a91b2299972
mecreal <annoncecbh@gmail.com>
2023-03-21 16:31:08 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_print_last_digit - function to return last digit
+ * @c: a number to be checked
+ * Return: 1 - if lowercase 0 id otherwise
+*/
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
