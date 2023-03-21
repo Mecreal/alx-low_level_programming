8afef29d10ed4772995a9833052e92179a166bfe
mecreal <annoncecbh@gmail.com>
2023-03-21 09:06:06 -0700

    function alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - see bellow
+ * function to print alphabet 10 times whitout any return_
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+	int i=0;
+	while(i<10){
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+		i++;
+	}
+}
