1c995bdbf206f084471c1386355a6d456e4b95c9
mecreal <annoncecbh@gmail.com>
2023-03-21 15:35:39 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,15 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
+		_putchar ('+');
 		return (1);
-	}else if (c < 0)
+	} else if (c < 0)
 	{
+		_putchar('-');
 		return (-1);
-	}else
+	} else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
