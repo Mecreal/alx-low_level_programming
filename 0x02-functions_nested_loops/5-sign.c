fbb6c3f10e628acbdd7f7b5305e5b52e03678066
mecreal <annoncecbh@gmail.com>
2023-03-21 10:58:48 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - check if it is an alphabet
+ * @c: a number to be checked
+ * Return: 1 - if lowercase 0 id otherwise
+*/
+
+int print_sign(int c)
+{
+	switch (c)
+	{
+		case c > 0:
+			return (1);
+		case c < 0:
+			return (-1);
+		case c == 0:
+			return (1);
+}
