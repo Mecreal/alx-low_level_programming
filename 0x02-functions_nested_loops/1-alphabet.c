fe49f63f486655bd9922d3fe360f816615d0d2ec
mecreal <annoncecbh@gmail.com>
2023-03-21 08:54:52 -0700

    function alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - see bellow 
+ * function to print alphabet whitout any return_
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar('ch');
+	_putchar('\n');
+}
