f8ef8f806d488e7ce07e75d677c6933dafc130cd
mecreal <annoncecbh@gmail.com>
2023-03-21 20:07:26 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_abs -calculate the absolut value
+ * @c: a number to be checked
+ * Return: 1 - if lowercase 0 id otherwise
+*/
+
+int _add(int a, int b)
+{
+	return (a + b);
+}
