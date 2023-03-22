5e40ba1e0e27d812b43fb99c547786fa343a5673
mecreal <annoncecbh@gmail.com>
2023-03-21 20:09:01 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _add -calculate the additions value
+ * add -calculate the additions value
  * @a: a number to be checked
  * @b: seconde number
  * Return: 1 - if lowercase 0 id otherwise
 */
 
-int _add(int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
