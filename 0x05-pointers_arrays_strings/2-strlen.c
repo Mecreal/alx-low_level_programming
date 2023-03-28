f4ae1ee65ba0b3aa5f5e97da142e8d035b52cd8b
mecreal <annoncecbh@gmail.com>
2023-03-27 19:09:45 -0700

    length of t string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - function calculating the length of a string
+* @s : the pointer of the string
+* Return: lenght
+*/
+
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (c);
+}
