3d8c72ad27e64cabb9cc101d2affa79456f3fd4c
mecreal <annoncecbh@gmail.com>
2023-04-01 00:38:06 -0700

    initialization

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - tests function that prints if integer is positive or negative
+* @n : point to the variable needed
+*/
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
