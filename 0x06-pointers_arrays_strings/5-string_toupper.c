6caef9c76d7d5abf12455b804bd22dc94d626df8
mecreal <annoncecbh@gmail.com>
2023-04-03 22:47:47 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* string_toupper - Uppercase
+* @s : the string
+* Return - the string in uppercase
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		s[i] = (s[i] >= 'a' && s[i] <= 'z' ? s[i] - 32 : s[i]);
+		i++;
+	}
+	return (s);
+}
