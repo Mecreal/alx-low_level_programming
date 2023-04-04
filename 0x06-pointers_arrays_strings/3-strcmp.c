777968c0c4e6e5cd6d02e8b632a715e57a8e504e
mecreal <annoncecbh@gmail.com>
2023-04-03 21:58:33 -0700

    3-corrected

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	int c1 = 0, c2 = 0;
+	int i = 0;
+	int c1 = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		c1 += s1[i];
+		c1 += s1[i] - s2[j];
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		c2 += s2[j];
-		j++;
-	}
-	return (c1 - c2);
+	return (c1);
 }
