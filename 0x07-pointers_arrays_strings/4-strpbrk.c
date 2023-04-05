2bb6cf7df85a71d33c3eb7d221bd2f46bfbf0928
mecreal <annoncecbh@gmail.com>
2023-04-04 19:59:23 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _strpbrk -  function that searches a string for any of a set of bytes.
+* @accept : consist only of bytes
+* @s : string to be add to dest
+* Return:  the number of bytes in the initial segment of s which consist
+* only of bytes from accept
+*/
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int l = 0;
+
+	unsigned int i = 0;
+
+	while (s[l] != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (s[l] == accept[i])
+				return (s);
+			i++;
+		}
+		l++;
+	}
+	return ('\0');
+}
