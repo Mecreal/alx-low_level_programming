cb913fbbb9945520347defef96e520e830526244
mecreal <annoncecbh@gmail.com>
2023-04-04 18:37:30 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_number - printing a number.
+* @s: the integer to be printed
+* Return: the string in uppercase
+*/
+
+void print_number(int n)
+{
+	unsigned int x;
+	int div = 1, mod = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -n;
+	} else{
+		x = n;
+	}
+	while (x / div > 9)
+		div *= 10;
+	while( div > 0)
+	{
+		x /= 10;
+		mod = x % 10;
+		_putchar('0' + mod);
+		div /= 10;
+	}
+	_putchar('\n');
+}
