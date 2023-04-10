aad4d6d7eb4cf74c92df98db9480ccc89f1ff793
mecreal <annoncecbh@gmail.com>
2023-04-10 16:32:29 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+*  main - the main function fot the name code
+* @argc : number of input in arguments
+* @argv : the arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
