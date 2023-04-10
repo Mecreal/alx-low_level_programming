a97657f4dcc259182d9f115ecb8a8dee0f41fbe1
mecreal <annoncecbh@gmail.com>
2023-04-10 16:41:26 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+*  main - the main function fot the name code
+* @argc : number of input in arguments
+* @argv : the arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	char *mute = *argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
