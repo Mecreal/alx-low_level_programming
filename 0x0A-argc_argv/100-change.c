a276a4091268c0156cb52ef8a872f894421a7717
mecreal <annoncecbh@gmail.com>
2023-04-10 18:02:53 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+/**
+*  main - the main function fot the name code
+* @argc : number of input in arguments
+* @argv : the arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+		printf("0\n");
+	else
+	{
+	
+	}
+	return (0);
+}
