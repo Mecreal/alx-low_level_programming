14538ced0f6e9f7bba7ebc7a64500d1631ceb0dd
mecreal <annoncecbh@gmail.com>
2023-04-10 17:28:34 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+/**
+*  main - the main function fot the name code
+* @argc : number of input in arguments
+* @argv : the arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int multi = 1;
+
+	if (argc == 3)
+	{
+		multi = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multi);
+	} else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
