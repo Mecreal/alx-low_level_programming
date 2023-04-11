eff7826f4cf324693546a871cbecb709247f3b06
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-11 17:14:17 +0000

    0-corrected

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,18 +12,20 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-    int i = 0;
+	unsigned int i = 0;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
 	arr = malloc(sizeof(c) * size);
 	if (arr != NULL)
 	{
-        while(arr[i] != 0)
-            arr[i] = c;
-            i++;
-        return (arr);
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+		return (arr);
 	}
 	return (NULL);
 }
