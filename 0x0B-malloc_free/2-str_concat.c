0fe8180f9ba6eb072caa2503023e45f1f33d3e35
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-11 20:48:02 +0000

    2-corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,12 +22,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		l2 = 0;
 		s2 = "";
-	} 
+	}
 	while (*(s1 + l1) != '\0')
 		l1++;
 	while (*(s2 + l2) != '\0')
 		l2++;
-	
+
 	lt = l1 + l2;
 	concat = malloc(sizeof(char) * (lt + 1));
 	if (concat != NULL)
