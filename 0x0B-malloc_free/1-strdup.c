a29d50737380aa83cc7d6411c764fc402d283a0f
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-11 16:57:43 +0000

    0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * create_array- creating an array object
+ * @size: the size of the variable
+ * @c: the character
+ * Return: char* or a NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int *arr;
+
+	arr = malloc(sizeof(c) * size);
+	if (arr != NULL)
+	{
+		return (arr);
+	}
+	return (NULL);
+}
