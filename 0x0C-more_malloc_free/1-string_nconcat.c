79be3fe20f6d133b6b896b690a51fc8e95626345
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-14 02:34:53 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,19 +3,53 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - function that allocates memory using malloc
- * @b: the size of the the space allocated
+ * string_nconcat - function that allocates memory using malloc
+ * @n: first n bytes of s2, and null terminated
+ * @s1: string 1
+ * @s2: string 2
  * Return: char* or a NULL
  */
 
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *mal_check;
+	char *concat;
+	unsigned int l1 = 0,  l2 = 0, lt = 0, i = 0;
 
-	mal_check = malloc(b);
-	if (mal_check == NULL)
+	if (s1 == NULL)
 	{
-		exit(98);
+		s1 = "";
+	} else
+		while (*(s1 + l1) != '\0')
+			l1++;
+	if (s2 == NULL)
+	{
+		s2 = "";
+	} else
+		while (*(s2 + l2) != '\0')
+			l2++;
+	if (l2 >= n)
+		lt = l1 + n;
+	else
+	{
+		lt = l2 + l1;
+		n = l2;
 	}
-	return (mal_check);
+	concat = malloc(sizeof(char) * (lt + 1));
+	if (concat != NULL)
+	{
+		while (*(s1 + i) != '\0' && l1 != 0)
+		{
+			*(concat + i) = *(s1 + i);
+			i++;
+		}
+		i = 0;
+		while (i < n && l2 != 0)
+		{
+			concat[l1 + i] = s2[i];
+			i++;
+		}
+		*(concat + lt + 1) = '\0';
+		return (concat);
+	}
+	return (NULL);
 }
