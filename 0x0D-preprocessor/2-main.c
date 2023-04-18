7a2a2e1a24917c2e04bbdb5c82021d3687ee3357
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-18 00:26:17 +0000

    2 task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#ifndef __FILE__
+#define __FILE__
+
+#endif
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%s\n",__FILE__);
+    return (0);
+}
