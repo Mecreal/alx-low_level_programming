292bfc511f19af9ccf67f361778c47318de39df8
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-18 02:00:02 +0000

    3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef _function_like_macro_h_
 #define _function_like_macro_h_
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) (((x) < (0)) ? (-x) : (x))
 #endif /* _function_like_macro_h_ */
