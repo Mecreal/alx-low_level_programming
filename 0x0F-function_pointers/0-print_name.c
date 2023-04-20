a39c5874a594a5fa213082beb85458a2ff5d9471
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-20 07:03:04 +0000

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that return another function
+ * @name: the argument to be sent to name_fun
+ * @name_fun: the callback function
+ */
+
+void print_name(char *name, void (*name_fun)(char *))
+{
+	if (name == NULL || name_fun == NULL)
+	{
+		exit(98);
+	}
+    (*name_fun)(name);
+
+}
