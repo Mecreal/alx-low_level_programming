651e61a56f6801c0a6108299d8729315a9c12454
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-20 07:10:04 +0000

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,4 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "function_pointers.h"
 /**
  * print_name - function that return another function
  * @name: the argument to be sent to name_fun
