81d771c5ac9e0db820b738838426abdeb23c69fd
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-04-28 03:27:15 +0100

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (head != NULL) {
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
