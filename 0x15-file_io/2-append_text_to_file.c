d13f239bcbdd8097a43ba9964fd46c98ab6f0d4f
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-09-05 03:56:08 +0100

    file_io_3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,53 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * append_text_to_file -  function that appends text at the end of a file.
- * @filename:  the name of the file to create
- * @text_content: is a NULL terminated string to write to the file
- * Return: success 1 faillure -1
- */
-int append_text_to_file(const char *filename, char *text_content)
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
 {
-	int num_char = 0;
-	int file;
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
+char buf[1024];
 
-
-	if (!filename)
-		return (-1);
-
-	if (!text_content)
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(file_from), exit(99);
+}
+while (num1 == 1024)
+{
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
 	{
-		text_content = "";
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 
-	file = open(filename, O_WRONLY | O_APPEND);
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-	if (file == -1)
-		return (-1);
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
-	while (text_content[num_char] != '\0')
-	{
-		num_char++;
-	}
-	if (write(file, text_content, num_char) == -1)
-		return (-1);
-	close(file);
-	return (1);
+return (0);
 }
