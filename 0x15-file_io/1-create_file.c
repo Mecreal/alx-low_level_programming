0019c0b51c9686dc86c0ab108a274286a9de451b
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-09-05 03:31:19 +0100

    file_io_1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		num_char = 0;
 	} else
 	{
-		while (text_content[i] != '\0')
+		while (text_content[num_char] != '\0')
 		{
 			num_char++;
 		}
