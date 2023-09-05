1f5c5170131a355518facf81d6c1a29a9bb1cfe2
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-09-05 03:37:00 +0100

    file_io_2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * create_file -  function that appends text at the end of a file.
+ * @filename:  the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: success 1 faillure -1
+ */
+int int append_text_to_file(const char *filename, char *text_content)
+{
+	int num_char = 0;
+	int file;
+
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+	{
+		return (0);
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	while (text_content[num_char] != '\0')
+	{
+		num_char++;
+	}
+	if (write(file, text_content, num_char) == -1)
+		return (-1);
+
+	return (1);
+}
