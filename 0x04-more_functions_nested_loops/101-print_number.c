930a5257396b0c6972933f04b7b7e11d2891ba1e
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-21 10:16:48 +0100

    101 corrected

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,36 +5,16 @@
 */
 void print_number(int n)
 {
-	int temp = n;
+	unsigned int num = n;
 
-	int reversed = 0;
-	int last_digit;
-
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar('0');
-	} else
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-		}
-
-		while (temp != 0)
-		{
-			last_digit = temp % 10;
-
-			if (temp < 0)
-			{
-				last_digit = -last_digit;
-			}
-			reversed = reversed * 10 + last_digit;
-			temp /= 10;
-		}
-		while (reversed != 0)
-		{
-			_putchar('0' + reversed % 10);
-			reversed /= 10;
-		}
+		_putchar('-');
+		num = -num;
 	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
