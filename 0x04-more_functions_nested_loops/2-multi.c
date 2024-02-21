30ae88b1e0dcac39d24bd2fba48d783e6675e141
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-21 04:47:15 +0100

    multiplication function

---
+++ 0x04-more_functions_nested_loops/2-multi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - function that checks for a digit (0 through 9).
+* @a : the first number
+* @b : the seconde number
+* Return: returns a*b
+*/
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
